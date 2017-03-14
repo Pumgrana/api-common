3fd355ee91fc4a23999b65311131ca4f2aea32fc
moi <moi@exemple.com>
2017-03-14 17:24:35 +0100

    First Commit. Librarie Independante

---
+++ HistoryModel/lib/HistoryModel.rb
@@ -0,0 +1,8 @@
+require "HistoryModel/version"
+
+require_relative "app/models/history.rb"
+require_relative "app/controllers/histories_controller.rb"
+
+module HistoryModel
+  # Your code goes here...
+end
---
+++ HistoryModel/lib/HistoryModel/version.rb
@@ -0,0 +1,3 @@
+module HistoryModel
+  VERSION = "0.1.0"
+end
